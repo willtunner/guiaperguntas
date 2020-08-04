0150850af3ee19da9295a497a5df8c4693a02e96
william <williampereira21@gmail.com>
2020-08-03 21:40:30 -0300

    Começando a parte da resposta

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");//Importa o body parser
-const connection = require('./database/database');//Importa a conexão com o banco
-const Pergunta = require('./database/Pergunta');
+const bodyParser = require("body-parser");// Importa o body parser
+const connection = require('./database/database');// Importa a conexão com o banco
+const Pergunta = require('./database/Pergunta');// Model pergunta
 
-//Database 
+// Database 
 connection
     .authenticate()// Tenta se autenticar no banco
     .then(() => { // Caso consiga da a msg
@@ -14,11 +14,11 @@ connection
         console.log(msgErro);
     })
 
-//Estou dizendo para o Express usar o EJS como view engine
+// Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
-//Diz para o express onde está o arquivo statico 
-//Diz que aceita arquivos staticos css, imagem, JavaScript etc...
+// Diz para o express onde está o arquivo statico 
+// Diz que aceita arquivos staticos css, imagem, JavaScript etc...
 app.use(express.static('public'));
 
 // Diz: Envia os dados para o node em formato Json
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Diz: Pegar o valor vindo do formulario e codifica para o JS
 app.use(bodyParser.json());
 
-//Renderiza a página index dentro de views
+// Renderiza a página index dentro de views
 app.get("/", (req, res) =>{
     Pergunta.findAll({raw: true,order:[
         ['id','DESC']// ASC = Crescente || DESC = Decrescente
@@ -37,12 +37,12 @@ app.get("/", (req, res) =>{
     });
 });
 
-//Renderiza a página perguntar
+// Renderiza a página perguntar
 app.get("/perguntar", (req, res) =>{
     res.render("perguntar");
 });
 
-//Salva pergunta
+// Salva pergunta
 app.post("/salvarpergunta", (req, res) =>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
@@ -55,6 +55,23 @@ app.post("/salvarpergunta", (req, res) =>{
     }).then(() => {
         res.redirect("/");
     });
+});
+
+// Rota para pergunta
+//pega o id para fazer o select e buscar a pergunta
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;// Pega o id da pergunta passada pela rota
+    Pergunta.findOne({// Faz a busca de 1 registro no model pergunta
+        where: {id: id}// Quando o id for igual o id passado
+    }).then( pergunta => {// se der tudo certo
+        if(pergunta != undefined){// Verifica se veio algo diferente de nulo, encontrada
+            res.render("pergunta", {// Chama a página de pergunta
+                pergunta: pergunta // passa a pergunta para a view
+            }); 
+        }else{// Não foi encontrada
+            res.redirect("/");// Redireciona para a página inicial caso de erro ou não encontra
+        }
+    })
 });
 
 app.listen(4000,() => {console.log("App rodando!");});
